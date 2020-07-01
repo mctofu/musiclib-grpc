b61679ac7c58d2d3da3a3ec333ea9880c0299ccf
David McIntosh <804610+mctofu@users.noreply.github.com>
2020-07-01 00:47:07 -0700

    Add doc to scripts

--- cgo.sh
+++ cgo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-
+# Builds the cgo c client
 go build -o cgo/build/client.so -buildmode=c-shared cgo/client/client.go
--- protogen.sh
+++ protogen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-
+# Regenerates clients & server stubs from the grpc proto def
 docker-compose run --rm protoc /protobuf/bin/protoc -I/protobuf --go_out=plugins=grpc:/mlibgrpc/go/mlibgrpc --proto_path=/mlibgrpc /mlibgrpc/musiclib.proto
